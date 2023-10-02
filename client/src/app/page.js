e696b9c5859f312feacafcd38274bcabafec68bf
bikkubs97 <bikku4444@gmail.com>
2023-10-02 10:07:30 +0530

    new

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,7 @@
 import Products from "./Components/Products";
 
 export default async function Home() {
+  //get products data from server
   const res = await fetch("https://friends-merch-server.onrender.com/products");
   const productsData = await res.json()
 
